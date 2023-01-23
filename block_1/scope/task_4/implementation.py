bfd26afab9726e9c39c0c763762776abcab768b1
Диана <d.nechunaeva@bars.group>
2023-01-23 23:12:43 +0300

    Урок 1.2

--- block_1/scope/task_4/implementation.py
+++ block_1/scope/task_4/implementation.py
@@ -9,9 +9,10 @@ def open_and_close_file(file_path):
     Args:
         file_path: путь до файла
     """
-    open = bad_open
+    #open = bad_open
     ###
     # Добавьте свой код сюда
     ###
+
     f = open(file_path, 'r')
     f.close()
